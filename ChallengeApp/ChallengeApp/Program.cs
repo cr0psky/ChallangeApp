ce7784873f1e1a7c4ca50f50edeec6b3a5cef6b8
krzysztof.krupski <krzysztof.krupski@gmail.com>
2023-03-18 21:37:18 +0100

    Zadanie domowe - Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+char myVar1 = 'K';
+var age = 33;
+
+if (myVar1 == 'K' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (myVar1 != 'K' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else if (age > 33)
+{
+    Console.WriteLine("Pozostałe osoby");
+}
